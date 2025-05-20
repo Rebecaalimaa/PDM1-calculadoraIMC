4c531550c0d4a41bc2c6066dfe9485739aaa6ffa
Rebecaalimaa <rebecalimanascimento3@gmail.com>
2025-05-20 14:11:56 -0300

    app.login

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -1,52 +1,21 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  final double peso;
-  final double altura;
-
-  const HomeScreen({super.key, required this.peso, required this.altura});
-
-  double calcularIMC() {
-    return peso / (altura * altura);
-  }
-
-  String classificarIMC(double imc) {
-    if (imc < 18.5) return 'Abaixo do peso';
-    if (imc < 24.9) return 'Peso normal';
-    if (imc < 29.9) return 'Sobrepeso';
-    if (imc < 34.9) return 'Obesidade grau 1';
-    if (imc < 39.9) return 'Obesidade grau 2';
-    return 'Obesidade grau 3';
-  }
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    double imc = calcularIMC();
-    String classificacao = classificarIMC(imc);
-
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Resultado IMC'),
-        backgroundColor: Colors.blueGrey,
-      ),
+      appBar: AppBar(title:Text('Tela Principal'),backgroundColor: Colors.blueGrey),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
-              'Seu IMC é: ${imc.toStringAsFixed(2)}',
+              'Tela Principal',
               style: TextStyle(fontSize: 22, color: Colors.blueGrey),
             ),
-            Text(
-              'Classificação: $classificacao',
-              style: TextStyle(fontSize: 18),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Voltar'),
-            )
+            ElevatedButton(onPressed: (){}, child: Text('Sair'))
           ],
         ),
       ),
